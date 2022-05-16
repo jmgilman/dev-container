c8785662d31546128829b16d5f7d6a9fec1e9960
Joshua Gilman <joshuagilman@gmail.com>
2022-05-16 19:41:12 +0000

    build: adds custom zsh function for creating conventional commits

--- flake.nix
+++ flake.nix
@@ -8,14 +8,30 @@
   outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        # Inherit system packages
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        # Configure home-manager overrides
+        hm = pkgs.lib.generators.toPretty {} {
+          programs.zsh = {
+            envExtra = ''
+              # Custom functions
+              cc() {
+                cog commit $@ && git commit --amend --no-edit
+              }
+            '';
+          };
+        };
+
+        # Create a wrapper for enforcing conventional commit messages
         checkCommit = pkgs.writeShellScriptBin "checkCommit" ''
           cog verify "$(cat $1)"
         '';
       in
       {
+        # Add custom pre-commit checks
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
@@ -31,12 +47,28 @@
             };
           };
         };
+
+        # Configure development environment
         devShell = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          inherit (self.checks.${system}.pre-commit-check);
+          shellHook = ''
+            ${self.checks.${system}.pre-commit-check.shellHook}
+            # Push local home-manager config to filesystem
+            cat <<EOT > /tmp/local.nix
+            { config, pkgs, ... }:
+              ${hm}
+            EOT
+
+            # If something has changed, replace the config and call switch
+            if ! cmp --silent -- /tmp/local.nix ~/.config/devcontainer/extra/local.nix; then
+              cp /tmp/local.nix ~/.config/devcontainer/extra/local.nix
+              home-manager switch --flake ~/.config/devcontainer#vscode
+            fi
+          '';
           packages = [
             checkCommit
-            pkgs.hadolint
             pkgs.cocogitto
+            pkgs.hadolint
           ];
         };
       }
